956e9449c70ee35e5b797cb18e8ffe1d1511bc7b
u4usmangenius <usmantahir.gc.33@gmail.com>
2023-07-16 10:07:18 +0500

    Shop Management System

---
+++ Shop Management System.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <iomanip>
+#include<conio.h>
+#include<fstream>
+#include "main.h"
+using namespace std;
+main()
+{
+	cout<<"good job,MASHALLAH!!!"<<endl;
+	maiin *m1;
+	m1->mainmenu();
+}
